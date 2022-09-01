52da61639746c56a822a83b05c6803df9b2208ff
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-01 09:13:29 +0200

    add initial sources
    Public headers are going into a `include/` subfolder, and C++ code
    source into a `src/` subfolder.

---
+++ include/conception-orientee-objet/example-adder.hpp
@@ -0,0 +1,9 @@
+#ifndef CONCEPTION_ORIENTEE_OBJET_EXAMPLE_ADDER_HPP
+#define CONCEPTION_ORIENTEE_OBJET_EXAMPLE_ADDER_HPP
+
+namespace coo {
+auto add(long first, long second) -> long;
+auto sub(long first, long second) -> long;
+} // namespace coo
+
+#endif
---
+++ src/example-adder.cpp
@@ -0,0 +1,7 @@
+#include "conception-orientee-objet/example-adder.hpp"
+
+namespace coo {
+auto add(const long first, const long second) -> long { return first + second; }
+
+auto sub(const long first, const long second) -> long { return first - second; }
+} // namespace coo
